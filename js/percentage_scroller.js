6ddd8aef116546cb6ce6956b4191b10d5b6fbb92
MorenoRT1402 <morenort1402@gmail.com>
2024-07-16 10:35:50 +0200

    feat: percentage scroller interact

---
+++ js/percentage_scroller.js
@@ -0,0 +1,12 @@
+const scroller = document.getElementById('percentage-scroller');
+
+const updateProgressBar = () => {
+    const scrollablePixels = document.body.scrollHeight - window.innerHeight;
+    scroller.style.width = `${ window.scrollY / scrollablePixels * 100 }%`;
+}
+
+window.addEventListener ('scroll', () => {
+    requestAnimationFrame(updateProgressBar());
+});
+
+console.log(scroller);

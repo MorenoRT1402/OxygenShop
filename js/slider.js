f1fc947c5551d276addac939163d5a595ec78787
MorenoRT1402 <morenort1402@gmail.com>
2024-07-17 16:14:29 +0200

    refactor : slider lit decoupling

--- js/slider.js
+++ js/slider.js
@@ -13,28 +13,42 @@ class Slider {
         this.imgDisplayer = document.getElementById('slider__img-displayer');
         this.imgIndex = mainID;
         this.imgSummaryContainer = document.getElementById('slider__img-summary');
+        this.points = [];
         this.initSlider();
+    }
+
+    updatePoints(){
+        this.points[this.imgIndex].classList.toggle('current');
     }
 
     updateSlider(){
         const currentImg = `${imagesPath}${images[this.imgIndex]}`;
-        console.log(currentImg);
         this.imgDisplayer.src = currentImg;
+        this.updatePoints();
     }
 
     createImgSummaries(){
-        images.forEach((image, index) => {
+        images.forEach(() => {
             const point = document.createElement('button');
             point.classList.add('slider__images-summary__button');
-            if(index == this.imgIndex){
-                point.classList.add('current');
-            }
+
+            this.points.push(point);
             this.imgSummaryContainer.appendChild(point);
         })
     }
 
     initSlider(){
         this.createImgSummaries();
+        this.updateSlider();
+    }
+
+    move(displacement){
+        this.updatePoints();
+
+        this.imgIndex += displacement;
+        if(this.imgIndex >= images.length) this.imgIndex = 0;
+        if(this.imgIndex < 0) this.imgIndex = images.length -1;
+
         this.updateSlider();
     }
 }
